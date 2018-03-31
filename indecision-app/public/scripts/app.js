483b093aa23b8ffe3e46606e2aeb117a9a9a4d88
isaacgr <isaacgrowell@gmail.com>
2018-03-30 20:13:57 -0400

    Modify app.js to be rendered by babel from src/app.js

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,14 +1,80 @@
+'use strict';
+
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
-// JavaScript syntax extention
+var app = {
+  title: 'Indecision',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
 
-//var template = <p>This is JSX from app.js</p>;
-
+//App Title
 var template = React.createElement(
-  "h1",
+  'div',
   null,
-  "This is JSX from app.js"
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      app.options[0]
+    ),
+    React.createElement(
+      'li',
+      null,
+      app.options[1]
+    )
+  )
+);
+
+var user = {
+  name: 'Isaac',
+  age: 26,
+  location: 'Hamilton'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+};
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
