5e0c4a031d635e187421350e50cb0d32cb814b60
isaacgr <isaacgrowell@gmail.com>
2018-04-01 15:31:37 -0400

    Move initial jsx indecision code to playground folder

---
+++ indecision-app/src/playground/jsx-indecision.js
@@ -0,0 +1,57 @@
+const app = {
+  title: 'Indecision',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value; //points to the element that the event started on
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderDom();
+  }
+}
+
+const removeAllOptions = () => {
+  app.options = [];
+  renderDom();
+}
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random()*app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+  console.log(randomNum);
+}
+
+const appRoot = document.getElementById('app');
+
+const renderDom = () =>{
+  const template = (
+    <div>
+      <h1>
+        {app.title}
+      </h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={removeAllOptions}>Remove All</button>
+      <ol>
+        {app.options.map((option) => {
+          return <li key={option}>{option}</li>
+        })}
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"></input>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, appRoot);
+}
+
+renderDom();
