48ac5487ba24e2ccafb925def5f858cc6d4498ed
Isaac Rowell <irowell@evertz.com>
2018-04-04 15:12:26 -0400

    Store and retrieve count using local storage

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -5,8 +5,21 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this, props);
     this.state = {
-      count: props.count
+      count: 0
     };
+  }
+  componentDidMount() {
+    const stringCount = localStorage.getItem('count');
+    const count = parseInt(stringCount);
+
+    if (!isNaN(count)){
+      this.setState(() => ({count}));
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count){
+      localStorage.setItem('count', this.state.count)
+    }
   }
   handleAddOne(){
     this.setState((prevState) => {
@@ -22,10 +35,10 @@ class Counter extends React.Component {
       }
     });
   }
-  handleReset(props) {
+  handleReset() {
     this.setState(() => {
       return {
-        count: props.count
+        count: 0
       }
     });
   }
@@ -41,37 +54,4 @@ class Counter extends React.Component {
   }
 }
 
-Counter.defaultProps = {
-  count: 0
-}
-
 ReactDOM.render(<Counter />, document.getElementById('app'));
-
-// let count = 0;
-// const addOne = () => {
-//   count++;
-//   renderCounterApp();
-// };
-// const minusOne = () => {
-//   count--;
-//   renderCounterApp();
-// };
-// const reset = () => {
-//   count = 0;
-//   renderCounterApp();
-// };
-//
-//
-// const renderCounterApp = () => {
-//   const templateTwo = (
-//     <div>
-//       <h1>Count: {count}</h1>
-//       <button onClick={addOne}>+1</button>
-//       <button onClick={minusOne}>-1</button>
-//       <button onClick={reset}>Reset</button>
-//     </div>
-//   );
-//   ReactDOM.render(templateTwo, appRoot);
-// };
-//
-// renderCounterApp();
