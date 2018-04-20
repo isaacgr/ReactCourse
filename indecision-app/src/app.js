0bf0f9f43a854ba9ee70b679b0265ad2eed75331
isaacgr <isaacgrowell@gmail.com>
2018-04-19 22:28:50 -0400

    Import normalize.css Import styles.scss to render custom styles

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
-
-console.log('App.js is running!');
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
