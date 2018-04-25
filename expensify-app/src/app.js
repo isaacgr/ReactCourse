4359601b224f2c44b643e09c88ab43b6da06593f
isaacgr <isaacgrowell@gmail.com>
2018-04-25 07:09:31 -0400

    Add routes to expensify-app

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -2,5 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-ReactDOM.render(<p>Test</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    This is the dashboard
+  </div>
+)
+
+const AddExpensePage = () => (
+  <div>
+    This is the expense page
+  </div>
+)
+
+const EditExpensePage = () => (
+  <div>
+    This is the edit page
+  </div>
+)
+
+const HelpPage = () => (
+  <div>
+    This is the help page
+  </div>
+)
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route
+        path='/'
+        component={ExpenseDashboardPage}
+        exact={true}
+      />
+      <Route
+        path='/create'
+        component={AddExpensePage}
+      />
+      <Route
+        path='/edit'
+        component={EditExpensePage}
+      />
+      <Route
+        path='/help'
+        component={HelpPage}
+      />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
