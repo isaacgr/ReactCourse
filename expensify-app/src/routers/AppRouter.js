a35ab95df134cb6dc18a9ea228b25b8055aba919
isaacgr <isaacgrowell@gmail.com>
2018-04-26 07:29:43 -0400

    Breakout router components to seperate files

---
+++ expensify-app/src/routers/AppRouter.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Header from '../components/Header';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import NotFoundPage from '../components/NotFoundPage';
+import HelpPage from '../components/HelpPage';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+  NavLink
+} from 'react-router-dom';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header/>
+      <Switch>
+        <Route
+          path='/'
+          component={ExpenseDashboardPage}
+          exact={true}
+        />
+        <Route
+          path='/create'
+          component={AddExpensePage}
+        />
+        <Route
+          path='/edit'
+          component={EditExpensePage}
+        />
+        <Route
+          path='/help'
+          component={HelpPage}
+        />
+        <Route
+          component={NotFoundPage}
+        />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
