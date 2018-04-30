b85f53f6936cf9903dcbe02d8de7a68a62e9a832
isaacgr <isaacgrowell@gmail.com>
2018-04-30 06:43:46 -0400

    Use redux store in app.js

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -3,5 +3,31 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+
+const expenseOne = store.dispatch(addExpense({
+  description: 'Water Bill',
+  amount: 100,
+  createdAt: 1000
+}));
+
+const expenseTwo = store.dispatch(addExpense({
+  description: 'Gas Bill',
+  amount: 200,
+  createdAt: 1001
+}));
+
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
