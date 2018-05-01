95aabaf7f5553f38bc212a55be28ecd1f023371e
isaacgr <isaacgrowell@gmail.com>
2018-05-01 07:11:13 -0400

    Add expense list filtering and item removal

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { removeExpense } from '../actions/expenses';
 import { connect } from 'react-redux';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <div>
       <h3>{description}</h3>
       <p>Amount: {amount}</p>
       <p>Created At: {createdAt}</p>
+      <button onClick = {() => {
+        dispatch(removeExpense({id}));
+      }}>Remove</button>
     </div>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
