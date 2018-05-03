36ad31a8d2ee2f9a1854b580cec59c96bfb3701f
isaacgr <isaacgrowell@gmail.com>
2018-05-03 07:44:58 -0400

    Add form validation for expense Push expense data from ExpenseForm to dashboard page

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -5,15 +5,14 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const now = moment();
 
-console.log(now.format('L'));
-
 export default class ExpenseForm extends React.Component{
   state = {
     description: '',
     note: '',
     amount: '',
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ''
   };
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -25,20 +24,43 @@ export default class ExpenseForm extends React.Component{
   };
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({amount}))
     }
   };
   onDateChange = (createdAt) => {
-    this.setState(() => ({createdAt}))
+    if (createdAt){
+      this.setState(() => ({createdAt}))
+    }
   };
   onFocusChange = ({ focused }) => {
     this.setState(() => ({calendarFocused: focused}))
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.description || !this.state.amount) {
+      //set error state
+      this.setState(() => ({
+        error: 'Please Provide a description and an amount'
+      }))
+    } else {
+      //clear the error
+      this.setState(() => ({
+        error: ''
+      }));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      })
+    }
   }
   render(){
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type='text'
             placeholder='Description'
