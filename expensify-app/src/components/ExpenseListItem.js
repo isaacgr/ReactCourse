343955536b2e3573b472803e953a9c8a7f22a2ab
isaacgr <isaacgrowell@gmail.com>
2018-05-04 18:40:51 -0400

    Include ExpenseForm on EditExpensePage to allow user to edit expense Move remove button from dashboard page to EditExpensePage

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { removeExpense } from '../actions/expenses';
-import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
@@ -13,11 +11,8 @@ const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
         </Link>
       <p>Amount: {amount}</p>
       <p>Created At: {createdAt}</p>
-      <button onClick = {() => {
-        dispatch(removeExpense({id}));
-      }}>Remove</button>
     </div>
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
