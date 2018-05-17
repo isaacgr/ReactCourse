5213df34331daecbd7e6c23d0a529cb4adf8b008
isaacgr <isaacgrowell@gmail.com>
2018-05-17 07:23:06 -0400

    Modify amount to integer

--- expensify-app/src/tests/fixtures/expenses.js
+++ expensify-app/src/tests/fixtures/expenses.js
@@ -4,18 +4,18 @@ export default [{
   id: '1',
   description: 'Car',
   note: '',
-  amount: '100',
+  amount: 100,
   createdAt: moment(0)
 }, {
   id: '2',
   description: 'Rent',
   note: '',
-  'amount': '10000',
+  'amount': 10000,
   createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
   id: '3',
   description: 'Credit Card',
   note: '',
-  'amount': '1000',
+  'amount': 1000,
   createdAt: moment(0).add(4, 'days').valueOf()
 }]
