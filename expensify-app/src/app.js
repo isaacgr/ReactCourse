c90e86a87c1e163f00c98706ee932dcbb4fe7f2a
isaacgr <isaacgrowell@gmail.com>
2018-05-22 16:49:03 -0400

    Remove static expense values

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -12,28 +12,6 @@ import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-const expenseOne = store.dispatch(addExpense({
-  description: 'Water Bill',
-  amount: 500,
-  createdAt: 1000
-}));
-
-const expenseTwo = store.dispatch(addExpense({
-  description: 'Gas Bill',
-  amount: 200,
-  createdAt: 1001
-}));
-
-const expenseThree = store.dispatch(addExpense({
-  description: 'Rent',
-  amount: 109500,
-  createdAt: 2000
-}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
